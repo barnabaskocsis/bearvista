74067d99d661c5feca1cc474d05fb4ddae18437b
Barnabas Kocsis <barnabas.kocsis@adnovum.hu>
2024-09-02 20:49:48 +0200

    A12: improving fe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,20 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '~components/navigation/navigation.component';
 import { CustomSidenavComponent } from '~components/navigation/custom-sidenav/custom-sidenav.component';
 import { MatButton } from '@angular/material/button';
+import {
+  MatDrawer,
+  MatDrawerContainer,
+  MatDrawerContent,
+  MatSidenav,
+  MatSidenavContainer,
+  MatSidenavModule
+} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, CustomSidenavComponent, MatButton, RouterLink],
+  imports: [RouterOutlet,MatSidenavModule, NavigationComponent, CustomSidenavComponent, MatButton, RouterLink, MatDrawerContainer, MatDrawer,
+    MatDrawerContent, MatSidenav, MatSidenavContainer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
