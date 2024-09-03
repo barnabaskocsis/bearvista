e37fd6ec253783e02db38413a7957ec0b9361a08
Barnabas Kocsis <barnabas.kocsis@adnovum.hu>
2024-09-03 20:18:24 +0200

    B-4: transloco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,44 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco, TranslocoService } from '@jsverse/transloco';
+import { firstValueFrom } from 'rxjs';
+import { TranslocoHttpLoader } from '~core/transloco-loader';
+
+const default_language = 'en';
+
+function preloadTranslation(transloco: TranslocoService) {
+  return async function () {
+    transloco.setActiveLang(default_language);
+    return await firstValueFrom(transloco.load(default_language));
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: preloadTranslation,
+      multi: true,
+      deps: [TranslocoService],
+    },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideStore()]
+    provideStore(),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en'],
+        defaultLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    })
+  ]
 };
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component, computed, effect, signal } from '@angular/core';
 import { MatButton, MatButtonModule } from '@angular/material/button';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatButton
+    MatButton,
+    TranslocoPipe
   ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
